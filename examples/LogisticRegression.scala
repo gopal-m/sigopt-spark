144195e91a2e39f2d2981b91fbf97e77fa366a36
Gopal Mahadevan <gopal-m@users.noreply.github.com>
2016-08-26 17:32:11 -0700

    Update LogisticRegression.scala

--- examples/LogisticRegression.scala
+++ examples/LogisticRegression.scala
@@ -1,12 +1,47 @@
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.{Row, SQLContext}
-import org.apache.spark.sql.types.{DoubleType, StructField, StructType}
-import org.apache.spark.ml.evaluation.RegressionEvaluator
-import org.apache.spark.ml.regression.LinearRegression
-import org.apache.spark.ml.tuning.{ParamGridBuilder, CrossValidator}
+import org.apache.spark.mllib.classification.{LogisticRegressionModel, LogisticRegressionWithLBFGS}
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
+import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.util.MLUtils
-
 import com.sigopt.spark.SigOptCrossValidator
+
+// Load training data in LIBSVM format.
+val data = MLUtils.loadLibSVMFile(sc, "data/mllib/sample_libsvm_data.txt")
+
+// Split data into training (60%) and test (40%).
+val splits = data.randomSplit(Array(0.6, 0.4), seed = 11L)
+val training = splits(0).cache()
+val test = splits(1)
+
+// Run training algorithm to build the model
+val model = new LogisticRegressionWithLBFGS()
+  .setNumClasses(10)
+  .run(training)
+
+// Compute raw scores on the test set.
+val predictionAndLabels = test.map { case LabeledPoint(label, features) =>
+  val prediction = model.predict(features)
+  (prediction, label)
+}
+
+// Get evaluation metrics.
+val metrics = new MulticlassMetrics(predictionAndLabels)
+val accuracy = metrics.accuracy
+println(s"Accuracy = $accuracy")
+
+// Save and load model
+model.save(sc, "target/tmp/scalaLogisticRegressionWithLBFGSModel")
+val sameModel = LogisticRegressionModel.load(sc,
+  "target/tmp/scalaLogisticRegressionWithLBFGSModel")
+
+# import org.apache.spark.{SparkConf, SparkContext}
+# import org.apache.spark.sql.{Row, SQLContext}
+# import org.apache.spark.sql.types.{DoubleType, StructField, StructType}
+# import org.apache.spark.ml.evaluation.RegressionEvaluator
+# import org.apache.spark.ml.regression.LinearRegression
+# import org.apache.spark.ml.tuning.{ParamGridBuilder, CrossValidator}
+# import org.apache.spark.mllib.util.MLUtils
+
+
 
 object LogisticRegressionCV {
   def main(args: Array[String]): Unit ={
